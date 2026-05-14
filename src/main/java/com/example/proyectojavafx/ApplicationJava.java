ec0dc74514aff2023bcd115e6b6b19c27c5d68d7
dbertelli <useromegad@gmail.com>
2026-05-14 12:22:21 +0200

    Creacion del primer controlador y el menú login

--- src/main/java/com/example/proyectojavafx/ApplicationJava.java
+++ src/main/java/com/example/proyectojavafx/ApplicationJava.java
@@ -1,18 +1,17 @@
 package com.example.proyectojavafx;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ApplicationJava extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ApplicationJava.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Application!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,8 @@ module com.example.proyectojavafx {
     requires javafx.fxml;
     requires java.sql;
 
-
     opens com.example.proyectojavafx to javafx.fxml;
+    opens com.example.proyectojavafx.Controller to javafx.fxml;
+
     exports com.example.proyectojavafx;
 }
